e66091b01d45237332411d6fe7521d909452814e
Karan Sharma <karan413193sharma@email.com>
2026-01-16 12:01:41 +0530

    done

--- backend/news.js
+++ backend/news.js
@@ -4,6 +4,7 @@ const newsSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
+   public_id:String,
   createdAt: { type: Date, default: Date.now }
 });
 
